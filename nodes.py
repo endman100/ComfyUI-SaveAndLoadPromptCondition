e485172e4775a7233b52db06f30a2b6fe621bb84
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2026-04-26 20:28:40 +0200

    Update model path handling in nodes.py
    Refactor model paths to use base directory argument and ensure conditioning directory exists.

--- nodes.py
+++ nodes.py
@@ -6,17 +6,23 @@ from PIL import Image
 import numpy as np
 import torch
 import hashlib
+from comfy.cli_args import args
+from pathlib import Path
 
 # set the models directory
-if "conditionings" not in folder_paths.folder_names_and_paths:
-    current_paths = [os.path.join(folder_paths.models_dir, "conditionings")]
+
+if args.base_directory:
+    base_path = os.path.join(Path(os.path.abspath(args.base_directory)).parent.parent, "models")
 else:
-    current_paths, _ = folder_paths.folder_names_and_paths["conditionings"]
-folder_paths.folder_names_and_paths["conditionings"] = (current_paths, ".bin")
+    base_path = os.path.join(Path(os.path.dirname(os.path.realpath(__file__))).parent.parent, "models")
+
+os.makedirs(os.path.join(base_path, "conditionings"), exist_ok=True)
+
+folder_paths.folder_names_and_paths["conditionings"] = ([os.path.join(base_path, "conditionings")], [".bin"])
 
 class SaveConditioning:
     def __init__(self):
-        self.output_dir = folder_paths.get_output_directory()
+        self.output_dir = os.path.join(base_path, "conditionings")
 
     @classmethod
     def INPUT_TYPES(s):
